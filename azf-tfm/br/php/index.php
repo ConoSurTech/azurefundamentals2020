9da7fe4ec98ac3e2b7746152a52a054ff7135e80
Pablo Ariel Di Loreto <pablodiloreto@hotmail.com>
2020-09-12 17:17:45 -0300

    Added azf-tfm complete with asp.net projects & PHP for each region

---
+++ azf-tfm/br/php/index.php
@@ -0,0 +1,15 @@
+<html>
+    
+    <head>
+        <title>
+        Mi sitio web desde Este de Estados Unidos
+        </title>
+    </head>
+
+    <body>
+
+        <h1>Hola desde Este de Estados Unidos</h1>
+
+    </body>
+
+</html>
