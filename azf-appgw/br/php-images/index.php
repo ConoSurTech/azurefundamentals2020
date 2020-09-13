632523d1fd35c3beb275f8d749aadb14c6154d4e
Pablo Ariel Di Loreto <pablodiloreto@hotmail.com>
2020-09-13 19:00:19 -0300

    Added appgw folder

---
+++ azf-appgw/br/php-images/index.php
@@ -0,0 +1,15 @@
+<html>
+    
+    <head>
+        <title>
+        Mi sitio web desde el Sur de Brasil
+        </title>
+    </head>
+
+    <body>
+
+        <h1>Images desde el Sur de Brasil</h1>
+
+    </body>
+
+</html>
---
+++ azf-appgw/eus/php-video/index.php
@@ -0,0 +1,15 @@
+<html>
+    
+    <head>
+        <title>
+        Mi sitio web desde el Este de Estados Unidos
+        </title>
+    </head>
+
+    <body>
+
+        <h1>Video desde el Este de Estados Unidos</h1>
+
+    </body>
+
+</html>
